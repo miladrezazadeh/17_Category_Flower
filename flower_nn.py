ac6b349d72077a3c79d56393df6543af5511e2d0
Milad Rezazadeh <miladrezazadeh72@gmail.com>
2021-05-12 11:48:33 -0400

    load the datasets, images and targets with np.load

--- flower_nn.py
+++ flower_nn.py
@@ -1,12 +1,26 @@
 ## Name: Milad Rezazadeh
 ## SciNet username: rezaza10
 ## Description:
-##  Neural Network which will categorize a given flower image into its respective category
+##  Neural Network which will categorize a given flower image
+## into its respective category
+
+#################################################################
 
 ## Import required libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import keras.models as km, keras.layers as kl
+import os
+import urllib
 import warnings
 warnings.filterwarnings("ignore")
+
+#################################################################
+## Read the flower dataset, images and targets
+print("Reading flowers input file.")
+images = np.load('50x50flowers.images.npy')
+print("Reading flowers target file.")
+targets = np.load('50x50flowers.targets.npy')
+print(targets.shape)
+
