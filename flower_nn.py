dbc66e5930ab66d6906ce57bce3db81a485c53a5
Milad Rezazadeh <miladrezazadeh72@gmail.com>
2021-05-12 13:06:06 -0400

    splitting images and targets to training and testing datasets

--- flower_nn.py
+++ flower_nn.py
@@ -13,14 +13,27 @@ import matplotlib.pyplot as plt
 import keras.models as km, keras.layers as kl
 import os
 import urllib
+import sklearn.model_selection as skms
 import warnings
 warnings.filterwarnings("ignore")
 
 #################################################################
+
 ## Read the flower dataset, images and targets
 print("Reading flowers input file.")
 images = np.load('50x50flowers.images.npy')
 print("Reading flowers target file.")
 targets = np.load('50x50flowers.targets.npy')
-print(targets.shape)
+## targets are converted to np array (1360,1)
+targets = np.asarray(targets).reshape(1360,1)
+
+#################################################################
+
+## split data into training and test
+from sklearn.model_selection import train_test_split
+train_images, test_images, train_targets, test_targets = train_test_split(images,
+targets, train_size = 0.8, random_state = 42)
+
+################################################################3
+
 
